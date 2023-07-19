116f13b2f98b3de9dff45f0395b7c43233e404e2
Yuri Chechulin <yu.s.chechulin@gmail.com>
2023-07-19 18:01:19 +0500

    js

---
+++ tmp.js
@@ -0,0 +1,43 @@
+function sendRequest(publicKey = false) {
+    var publicKey = '6LcWXc8gAAAAAMpgB0-7TzTELlr8f7T2XiTrexO5';
+
+    getRecaptchaToken(publicKey, function (token) {console.log(token)}, errorCapture);
+}
+
+
+function makeAjaxCall1(token) {
+    var headers = { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' };
+    var url = 'https://rp.uremont.dev/result_rsa.php';
+    var data = '&gRecaptchaResponse=' + token;
+    fetch(url, {
+        method: 'POST',
+        headers: headers,
+        body: data
+    });
+
+
+    window.clearInterval(window.interval_submit_ajax);
+    window.interval_submit_ajax = window.setInterval(sendRequest, 60000);
+    //grecaptcha.reset();
+    //location.reload(true);
+
+    on
+
+    return false;
+}
+
+
+
+function errorCapture(err) {
+    return false;
+}
+
+
+function reload() {
+    location.reload(true);
+}
+
+
+$(document).ready(function () {
+    sendRequest();
+})
