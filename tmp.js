28b64950d0e025a22fbd40f05b715d004df62146
Yuri Chechulin <yu.s.chechulin@gmail.com>
2023-07-20 11:34:31 +0500

    tedst

--- tmp.js
+++ tmp.js
@@ -1,27 +1,25 @@
 function sendRequest(publicKey = false) {
-    var publicKey = '6LcWXc8gAAAAAMpgB0-7TzTELlr8f7T2XiTrexO5';
-
-    getRecaptchaToken(publicKey, function (token) {console.log(token)}, errorCapture);
+    // var publicKey = '6LcWXc8gAAAAAMpgB0-7TzTELlr8f7T2XiTrexO5';
+    //
+    // getRecaptchaToken(publicKey, function (token) {console.log(token)}, errorCapture);
 }
 
 
 function makeAjaxCall1(token) {
-    var headers = { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' };
-    var url = 'https://rp.uremont.dev/result_rsa.php';
-    var data = '&gRecaptchaResponse=' + token;
-    fetch(url, {
-        method: 'POST',
-        headers: headers,
-        body: data
-    });
-
-
-    window.clearInterval(window.interval_submit_ajax);
-    window.interval_submit_ajax = window.setInterval(sendRequest, 60000);
+    // var headers = { 'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8' };
+    // var url = 'https://rp.uremont.dev/result_rsa.php';
+    // var data = '&gRecaptchaResponse=' + token;
+    // fetch(url, {
+    //     method: 'POST',
+    //     headers: headers,
+    //     body: data
+    // });
+    //
+    //
+    // window.clearInterval(window.interval_submit_ajax);
+    // window.interval_submit_ajax = window.setInterval(sendRequest, 60000);
     //grecaptcha.reset();
     //location.reload(true);
-
-    on
 
     return false;
 }
@@ -39,5 +37,6 @@ function reload() {
 
 
 $(document).ready(function () {
-    sendRequest();
+    // sendRequest();
+    console.log('sfdsfsdfdsf');
 })
